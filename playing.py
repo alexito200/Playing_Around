7c1cdccdaf55cb5569df49d0c3e2a9d6196f3d9f
alex <alexalarcon200@gmail.com>
2024-09-10 20:37:24 -0400

    This will be the bridge between my laptop and my PC

---
+++ playing.py
@@ -0,0 +1,41 @@
+# Exercise 1 Movie Night Decision
+# ask user about current mood (happy, sad, adventurous)
+# ask user about day's weather (sunny/rainy)
+# determines movie genre based on above criteria
+# displays recommended movie genre to user
+
+#step 1 current mood
+
+# user_mood = input('Tell us your mood (happy, sad, adventurous): ')
+# user_weather = input('Tell us the weather for today (sunny, rainy): ')
+# 
+# if (user_mood == 'happy'):
+#     if (user_weather == 'sunny'):
+#         print('watch drama')
+#     elif (user_weather == 'rainy'):
+#         print ('watch rom com')
+#     else:
+#         pass
+# elif (user_mood == 'sad'):
+#     if (user_weather == 'sunny'):
+#         print('watch coming of age')
+#     elif (user_weather == 'rainy'):
+#         print ('watch horror')
+#     else:
+#         pass
+# elif (user_mood == 'adventurous'):
+#     if (user_weather == 'sunny'):
+#         print('watch upbeat')
+#     elif (user_weather == 'rainy'):
+#         print ('watch thriler')
+#     else:
+#         pass
+# else:
+#     print('go outside')
+
+if True: pass
+else: print("False")
+
+# x=10
+# y=20 
+# print("Greater" if x > y else "Smaller or Equal")
